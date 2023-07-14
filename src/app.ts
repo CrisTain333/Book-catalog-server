5c1361c91ad389a213832563b0e20f7e46d86436
Sukanta Das <arnobh84@gmail.com>
2023-07-14 09:35:07 +0600

    connect to database function and app + server created

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, {
+    Application,
+    NextFunction,
+    Request,
+    Response
+} from 'express';
+import cors from 'cors';
+// import router from './app/routes';
+// import globalErrorHandler from './middleware/errorHandeler';
+// import cookieParser from 'cookie-parser';
+
+// import router from './app/routes';
+// import globalErrorHandler from './middleware/globalErrorHandler';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+// app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+// Entrance
+// app.use('/api/v1', router);
+
+// Global Error handler
+// app.use(globalErrorHandler);
+
+// Handle Not found
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        success: false,
+        message: 'Not Found',
+        errorMessages: [
+            {
+                path: req.originalUrl,
+                message: 'Api Not Found'
+            }
+        ]
+    });
+    next();
+});
+
+export default app;
